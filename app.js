23f01370af9e46b27c203f8a9bc8a5c71774ccf0
95caparra <95caparra@misena.edu.co>
2020-04-13 19:52:45 -0500

    Primer Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//requires 
+var express = require('express');
+var mongoose = require('mongoose');
+
+//iniciar variables
+var app = express();
+
+//conexion a la bd
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', ' online');
+});
+
+//rutas
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada'
+    });
+});
+
+//escuchar petición
+app.listen(3000, () => {
+    console.log('Express server en el pueto 3000: \x1b[32m%s\x1b[0m', ' online');
+});

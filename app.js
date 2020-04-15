b313d2398dd75bf81470ccf52a7f7cabc91680ec
95caparra <95caparra@misena.edu.co>
2020-04-14 20:36:23 -0500

    usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 //requires 
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 //iniciar variables
 var app = express();
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 //conexion a la bd
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -11,13 +21,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
     console.log('Base de datos: \x1b[32m%s\x1b[0m', ' online');
 });
 
-//rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada'
-    });
-});
+//Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 //escuchar petición
 app.listen(3000, () => {
